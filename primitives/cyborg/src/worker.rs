a1e32ab2fb08ad62506366e8dd70e9183e5f8ec3
zcalz <calvinsze.coding@gmail.com>
2024-10-09 15:58:40 -0400

    feat: primitives

--- primitives/cyborg/src/worker.rs
+++ primitives/cyborg/src/worker.rs
@@ -1,4 +1,4 @@
-use codec::{Decode, Encode, MaxEncodedLen};
+use parity_scale_codec::{Decode, Encode, MaxEncodedLen};
 use frame_support::{pallet_prelude::ConstU32, sp_runtime::RuntimeDebug, BoundedVec};
 use scale_info::TypeInfo;
 
